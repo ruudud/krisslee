16af54a20ca5cd750c98d72956dc33d839225563
Pål Ruud <ruudud@gmail.com>
2010-05-12 09:50:29 +0000

    added multimedia and frontpage and styles and scripts

--- krisslee/urls.py
+++ krisslee/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
+    url(r'^$', 'krisslee.frontpage.views.front', name='frontpage'),
     (r'^admin/', include(admin.site.urls)),
     (r'^blog/', include('basic.blog.urls')),
     (r'comments/', include('django.contrib.comments.urls')),
