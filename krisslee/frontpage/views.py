438d28375f4558d215cbb7b92bb585ddc8bbfeb3
Pål Ruud <ruudud@gmail.com>
2010-05-21 18:35:27 +0000

    Major update

--- krisslee/frontpage/views.py
+++ krisslee/frontpage/views.py
@@ -1,9 +1,17 @@
 #encoding: utf-8
+import os
+
 from django.conf import settings
 from django.http import HttpResponseNotAllowed, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 def front(request):
-    return render_to_response('frontpage.html', {}, 
+    images = os.listdir(settings.MEDIA_ROOT + 'pics/')
+    pictures = [f for f in images if f[-3:] == 'jpg']
+
+    return render_to_response('frontpage.html', 
+        {
+            'pictures': pictures, 
+        }, 
         context_instance=RequestContext(request))
