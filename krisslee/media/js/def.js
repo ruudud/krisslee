454be247c676e8eaa993997c0a85ff398510d403
Pål Ruud <ruudud@gmail.com>
2010-05-26 10:03:05 +0000

    Added konami and updated keyb shortcuts of gallery

--- krisslee/media/js/def.js
+++ krisslee/media/js/def.js
@@ -19,8 +19,6 @@ $(document).ready(function() {
 
     /** Jiggle init **/
     jiggle('#regImg');
-    
-
 });
 
 function jiggle(selector) {
@@ -50,3 +48,22 @@ function padd(selector, new_dir) {
     }
     timers[timers.length] = window.setTimeout('padd(current_obj, dir)', 200);
 }
+
+(function($) {
+
+	$.fn.konami = function(callback, code) {
+		if(code == undefined) code = "38,38,40,40,37,39,37,39,66,65";
+		
+		return this.each(function() {
+			var kkeys = [];
+			$(this).keydown(function(e){
+				kkeys.push( e.keyCode );
+				if ( kkeys.toString().indexOf( code ) >= 0 ){
+					$(this).unbind('keydown', arguments.callee);
+					callback(e);
+				}
+			}, true);
+		});
+	}
+
+})(jQuery);
