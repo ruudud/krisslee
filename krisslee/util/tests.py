b302f661d412377f75ceda3454242f6f655983d5
Pål Ruud <ruudud@gmail.com>
2011-06-22 01:52:40 +0200

    Started on new menu look

---
+++ krisslee/util/tests.py
@@ -0,0 +1,12 @@
+from django.utils.unittest import TestCase
+from django.test.client import RequestFactory
+
+from krisslee.util.templatetags.url_utils import current
+
+class URLUtilTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_current_url_should_return_active(self):
+        request = self.factory.get('/')
+        self.failUnlessEqual('active', current(request, '/'))
