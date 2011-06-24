b35572b4a165335b35e2e1f0323e08fbee1b901c
Pål Ruud <ruudud@gmail.com>
2011-06-24 21:06:06 +0200

    Support for reverse urls in current templatetag

--- krisslee/util/tests.py
+++ krisslee/util/tests.py
@@ -1,3 +1,5 @@
+#coding: utf-8
+from django.core.urlresolvers import reverse
 from django.utils.unittest import TestCase
 from django.test.client import RequestFactory
 
@@ -11,3 +13,11 @@ class URLUtilTests(TestCase):
         request = self.factory.get('/')
         self.failUnlessEqual('active', current(request, '/'))
         self.failUnlessEqual('inactive', current(request, '/jukebox/'))
+
+    def test_current_reverse_url_should_return_active(self):
+        request = self.factory.get(reverse('frontpage'))
+        self.failUnlessEqual('active', current(request, 'frontpage'))
+
+    def test_missing_url_should_fail_with_inactive(self):
+        request = self.factory.get('/')
+        self.failUnlessEqual('inactive', current(request, 'frontpagezz'))
