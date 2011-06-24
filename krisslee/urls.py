73f2d6d7a101f0559eb8b1b33dae1aaa42b102d2
Pål Ruud <ruudud@gmail.com>
2011-06-24 20:52:07 +0200

    New menu design working.

--- krisslee/urls.py
+++ krisslee/urls.py
@@ -5,11 +5,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'krisslee.frontpage.views.front'),
+    url(r'^$', 'krisslee.frontpage.views.front', name='frontpage'),
     (r'^admin/', include(admin.site.urls)),
-    (r'^jukebox/', 'django.views.generic.simple.direct_to_template', {
+    url(r'^jukebox/', 'django.views.generic.simple.direct_to_template', {
             'template': 'jukebox.html', 
-        }),
+        }, name='jukebox'),
 )
 #Hack to get testserver to serve media
 if settings.DEBUG:
--- krisslee/util/templatetags/url_utils.py
+++ krisslee/util/templatetags/url_utils.py
@@ -4,5 +4,10 @@ from django import template
 
 register = template.Library()
 
-#@register.simple_tag
+@register.simple_tag
+def current(request, uri):
+    if request.path == uri:
+        return 'active'
+    else:
+        return 'inactive'
 
