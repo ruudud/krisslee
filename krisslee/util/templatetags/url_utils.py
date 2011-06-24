1018ffb8f975b921206c6b948f0cbb20921048db
Pål Ruud <ruudud@gmail.com>
2011-06-24 21:06:06 +0200

    Support for reverse urls in current templatetag

--- krisslee/util/templatetags/url_utils.py
+++ krisslee/util/templatetags/url_utils.py
@@ -1,13 +1,19 @@
 #coding: utf-8
-
 from django import template
+from django.core.urlresolvers import reverse, NoReverseMatch
 
 register = template.Library()
 
 @register.simple_tag
 def current(request, uri):
+    if not '/' in uri:
+        try:
+            uri = reverse(uri)
+        except NoReverseMatch:
+            return 'inactive'
+
     if request.path == uri:
         return 'active'
-    else:
-        return 'inactive'
+
+    return 'inactive'
 
